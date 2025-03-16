44945d7bd735cfdcda272dda25010297c54f7c7c
tureraomilind358 <tureraomilind358@gmail.com>
2025-03-16 20:16:42 +0530

    update db,js database name

--- src/config/db.js
+++ src/config/db.js
@@ -12,7 +12,7 @@ const pool = mysql.createPool({
 });
 
 // Database Name
-const databaseName = "clothing_store";
+const databaseName = "bvgx8oqh5wzniwimy7oz";
 
 // Function to Initialize Database & Tables
 const initializeDatabase = () => {
